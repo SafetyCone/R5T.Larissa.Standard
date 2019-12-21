3f4722029d9173ba6467e21296e06847f458ba60
MinexAutomation <minex.automation@gmail.com>
2019-12-21 14:08:43 -0800

    Added configuration-builder and service-collection extensions for the SVN operator.

---
+++ source/R5T.Larissa.Standard/Code/Extensions/IConfigurationBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Microsoft.Extensions.Configuration;
+using R5T.Larissa.Configuration.Suebia;
+
+
+namespace R5T.Larissa.Standard
+{
+    public static class IConfigurationBuilderExtensions
+    {
+        public static IConfigurationBuilder AddSvnConfiguration(this IConfigurationBuilder configurationBuilder, IServiceProvider configurationServiceProvider)
+        {
+            configurationBuilder.AddSvnConfigurationJsonFile(configurationServiceProvider);
+
+            return configurationBuilder;
+        }
+    }
+}
---
+++ source/R5T.Larissa.Standard/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+
+using R5T.Caledonia;
+using R5T.Caledonia.Default;
+using R5T.Larissa.Configuration;
+using R5T.Larissa.Configuration.Sardinia;
+using R5T.Larissa.Default;
+
+
+namespace R5T.Larissa.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSvnOperator(this IServiceCollection services)
+        {
+            services
+                .AddSingleton<ISvnOperator, SvnOperator>()
+                .AddSingleton<ISvnExecutableFilePathProvider, ConfigurationBasedSvnExecutableFilePathProvider>()
+                .AddSvnConfiguration()
+                .AddSingleton<ICommandLineInvocationOperator, DefaultCommandLineInvocationOperator>()
+                ;
+
+            return services;
+        }
+    }
+}
